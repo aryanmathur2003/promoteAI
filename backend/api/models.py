cdf12bc95d91863fd3fe1b4403e1a8409cfaf20c
Aryan Mathur <65492352+aryanmathur2003@users.noreply.github.com>
2024-01-21 05:10:59 -0500

    backend done

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class imageLinks(models.Model):
+    image1 = models.CharField(max_length=70)
+    image2 = models.CharField(max_length=70)
+    image3 = models.CharField(max_length=70)
+    image4 = models.CharField(max_length=70)
+    tweet1 = models.CharField(max_length=100)
+    tweet2 = models.CharField(max_length=100)
+    tweet3 = models.CharField(max_length=100)
+    tweet4 = models.CharField(max_length=100)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,37 @@
 from django.http import HttpResponse
+from .serializers import ImageSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import imageLinks
+from django.http import JsonResponse
+from django.http import HttpResponse
+from rest_framework import generics,status
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class createImage(APIView):
+    serializer_class = ImageSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            # Access the serialized data
+            serializer.save()
+            # image1 = serializer.data.get("image1")
+            
+            return Response({"message": "Data successfully processed"})
+        else:
+            return Response(serializer.errors, status=400)
+  
+class getImage(APIView):
+    def get(self, request):
+        list = imageLinks.objects.all()[0]
+        ans = {
+                'image1': list.image1,
+                'image2': list.image2,
+                'image3': list.image3,
+                'image4': list.image4,
+                'tweet1': list.tweet1,
+                'tweet2': list.tweet2,
+                'tweet3': list.tweet3,
+                'tweet4': list.tweet4,
+            }
+        list.delete()
+        return JsonResponse(ans, content_type='application/json', status=status.HTTP_200_OK)
